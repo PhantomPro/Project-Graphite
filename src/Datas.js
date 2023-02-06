272cea5107214759f35962c959ee6495812736aa
TANMAY BASU <basutanmay.007@gmail.com>
2023-02-06 10:12:00 +0530

    First Commit

---
+++ src/Datas.js
@@ -0,0 +1,105 @@
+let Datas = [
+  {
+    id: 1,
+    title: "Background",
+    routeAddress: [
+      {
+        id: 2,
+        route: "/backgroundcolor",
+        name: "Background Color",
+      },
+      {
+        id: 3,
+        route: "/backgroundgradient",
+        name: "Background Gradient",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Box",
+    routeAddress: [
+      {
+        id: 5,
+        route: "/border",
+        name: "Border",
+      },
+      {
+        id: 6,
+        route: "/BorderRadius",
+        name: "Border Radius",
+      },
+      {
+        id: 7,
+        route: "/box/boxshadow",
+        name: "Box-Shadow",
+      },
+      {
+        id: 8,
+        route: "/box/opasity",
+        name: "Opasity",
+      },
+    ],
+  },
+  
+  {
+    id: 19,
+    title: "Text",
+    routeAddress: [
+      {
+        id: 20,
+        route: "/text/fontsize",
+        name: "Font-size",
+      },
+      {
+        id: 21,
+        route: "/text/color",
+        name: "Text-color",
+      },
+      {
+        id: 22,
+        route: "/text/decoration",
+        name: "Text-decoration",
+      },
+      {
+        id: 23,
+        route: "/text/transform",
+        name: "Text-transform",
+      },
+      {
+        id: 24,
+        route: "/text/letterSpacing",
+        name: "Letter-Spacing",
+      },
+      {
+        id: 25,
+        route: "/text/lineheight",
+        name: "Line-height",
+      },
+      {
+        id: 26,
+        route: "/text/textalign",
+        name: "Text-align",
+      },
+      {
+        id: 27,
+        route: "/text/fontweight",
+        name: "Font-weight",
+      },
+    ],
+  },
+
+  {
+    id: 36,
+    title: "FlexBox",
+    routeAddress: [
+      {
+        id: 37,
+        route: "/flexbox",
+        name: "FlexBox",
+      },
+    ],
+  },
+];
+
+export default Datas;
---
+++ src/routes.js
@@ -0,0 +1,39 @@
+import Backgroundcolor from "./pages/BackgroundColor/Backgroundcolor";
+import Backgroundgradient from "./pages/Backgroundgradient/Backgroundgradient";
+import Border from "./pages/Border/Border";
+import BorderRadius from "./pages/BorderRadius/BorderRadius";
+import Home from "./pages/Home/Home";
+import BoxShadow from "./pages/Box/BoxShadow";
+import { Navigate } from "react-router-dom";
+import Opasity from "./pages/Box/Opasity";
+import FontSize from "./pages/Text/FontSize";
+import Textcolor from "./pages/Text/Textcolor";
+import TextDecoration from "./pages/Text/TextDecoration";
+import Texttransform from "./pages/Text/Texttransform";
+import LetterSpacing from "./pages/Text/LetterSpacing";
+import Lineheight from "./pages/Text/Lineheight";
+import Textalign from "./pages/Text/Textalign";
+import FontWeight from "./pages/Text/FontWeight";
+import FlexBox from "./pages/FlexBox/FlexBox";
+
+let router = [
+  { path: "/", element: <Home /> },
+  { path: "/backgroundcolor", element: <Backgroundcolor /> },
+  { path: "/backgroundgradient", element: <Backgroundgradient /> },
+  { path: "/border", element: <Border /> },
+  { path: "/BorderRadius", element: <BorderRadius /> },
+  { path: "/box/boxshadow", element: <BoxShadow /> },
+  { path: "/box/opasity", element: <Opasity /> },
+  { path: "/text/fontsize", element: <FontSize /> },
+  { path: "/text/color", element: <Textcolor /> },
+  { path: "/text/decoration", element: <TextDecoration /> },
+  { path: "/text/transform", element: <Texttransform /> },
+  { path: "/text/letterSpacing", element: <LetterSpacing /> },
+  { path: "/text/lineheight", element: <Lineheight /> },
+  { path: "/text/textalign", element: <Textalign /> },
+  { path: "/text/fontweight", element: <FontWeight /> },
+  { path: "/flexbox", element: <FlexBox /> },
+  { path: "*", element: <Navigate to={"/"} /> },
+];
+
+export default router;
